c6a3c6a794641cbd0fd25f1d9d23ead89e8f859e
larcis <uygarkoroglu@outlook.com>
2020-03-19 18:58:32 +0300

    bug fix

--- 16011052-hw2.pl
+++ 16011052-hw2.pl
@@ -10,11 +10,11 @@ sub init_dot_matrix { #line1 , line2, dot_matrix
         } 
     }
     $idx = 1;
-    foreach $i (split('', $_[0])){
+    foreach $i (split('', $_[1])){
          $_[2][0][$idx++] = $i;
     }
     $idx = 1;
-    foreach $j (split('', $_[1])){
+    foreach $j (split('', $_[0])){
         $_[2][$idx++][0] = $j;
     }
     
@@ -50,11 +50,11 @@ sub show_dot_matrix { # w, h , dot_matrix
     $img->fontsize($coeff/1.5);
 
     my ($i, $j);
-    for($i = 1; $i < $_[0] + 1; $i++){
+    for($i = 1; $i < $_[1] + 1; $i++){
         $img->moveTo($coeff/3, $i * $coeff + $coeff);
         $img->string($_[2][0][$i]);
     }
-    for($i = 1; $i < $_[1] + 1; $i++){
+    for($i = 1; $i < $_[0] + 1; $i++){
         $img->moveTo($i * $coeff+ $coeff / 3, $coeff);
         $img->string($_[2][$i][0]);
     }
@@ -125,9 +125,9 @@ do{
         chomp($line2);
         my @dot_matrix;
         init_dot_matrix($line1, $line2, \@dot_matrix);
-        #print_dot_matrix(length($line1), length($line2), \@dot_matrix);
+        print_dot_matrix(length($line1), length($line2), \@dot_matrix);
         fill_dot_matrix(length($line1), length($line2), \@dot_matrix);
-        #print_dot_matrix(length($line1), length($line2), \@dot_matrix);
+        print_dot_matrix(length($line1), length($line2), \@dot_matrix);
         show_dot_matrix(length($line1), length($line2), \@dot_matrix, "dot_matrix");
         print "\nplease enter window size to calculate noise: ";
         my $window_size = <>;
