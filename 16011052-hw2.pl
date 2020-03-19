91133a6ceee1c847e4bb9eaabb03b5b00e856122
larcis <uygarkoroglu@outlook.com>
2020-03-19 14:59:27 +0300

    noise computation added

--- 16011052-hw2.pl
+++ 16011052-hw2.pl
@@ -30,7 +30,18 @@ sub print_dot_matrix { # w, h, dot_matrix
     }
 }
 
-sub show_dot_matrix {
+sub clear_noise { # w, h, dot_matrix
+    my ($i, $j);
+    for($i = 0; $i < $_[0]+1; $i++) {
+        for($j = 0; $j < $_[1]+1; $j++){ 
+            if($_[2][$i][$j] eq 2){
+                $_[2][$i][$j] = 0;
+            }
+        }
+    }
+}
+
+sub show_dot_matrix { # w, h , dot_matrix
     my $coeff  = 2400 / $_[0]; 
     my $img = GD::Simple->new(($_[0] + 1) * $coeff, ($_[1] + 1) * $coeff); 
     $img->bgcolor('white');
@@ -47,11 +58,14 @@ sub show_dot_matrix {
         $img->moveTo($i * $coeff+ $coeff / 3, $coeff);
         $img->string($_[2][$i][0]);
     }
-    $img->bgcolor('black');
     $img->fgcolor('white');
     for($i = 1; $i < $_[0] + 1; $i++){
         for($j = 1; $j < $_[1] + 1; $j++){
             if($_[2][$i][$j] eq 1){
+                $img->bgcolor('black');
+                $img->rectangle($coeff*$i, $coeff*$j, $coeff*$i+$coeff, $coeff*$j+$coeff);
+            } elsif ($_[2][$i][$j] eq 2) {
+                $img->bgcolor('red');
                 $img->rectangle($coeff*$i, $coeff*$j, $coeff*$i+$coeff, $coeff*$j+$coeff);
             }
         }
@@ -76,11 +90,29 @@ sub fill_dot_matrix {  # w, h, dot_matrix
         }
     }
 }
-sub compute_noise {
-    
+sub compute_noise { # w, h, dot_matrix, window_size, stringency
+    my ($i, $j, $k);
+    my $noise = 0;
+    for($i = 1; $i < $_[0]+1 - $_[3]; $i++) {
+        for($j = 1; $j < $_[1]+1 - $_[3]; $j++) { 
+           my $sum = 0;
+           for($k = 0; $k < $_[3]; $k++) {
+               $sum += $_[2][$i + $k][$j + $k];
+           }
+           if($sum < $_[4]){
+                for($k = 0; $k < $_[3]; $k++) {
+                    if($_[2][$i + $k][$j + $k] eq 1){
+                        $noise++;
+                        $_[2][$i + $k][$j + $k] = 2;
+                    }
+                }
+           }
+        }
+    }
+    return($noise);
 }
 open(INPUT_FILE, "<", "input.txt") or die "Can't open input.txt file";
-open(OUTPUT_FILE, ">", "output.txt") or die "Can't open output.txt file";
+#open(OUTPUT_FILE, ">", "output.txt") or die "Can't open output.txt file";
 
 my $line1;
 my $line2;
@@ -97,5 +129,19 @@ do{
         fill_dot_matrix(length($line1), length($line2), \@dot_matrix);
         #print_dot_matrix(length($line1), length($line2), \@dot_matrix);
         show_dot_matrix(length($line1), length($line2), \@dot_matrix, "dot_matrix");
+        print "\nplease enter window size to calculate noise: ";
+        my $window_size = <>;
+        print "\nplease enter stringency: ";
+        my $stringency = <>;
+        my $noise = compute_noise(length($line1), length($line2), \@dot_matrix, $window_size, $stringency);
+        print "\nNoise is: $noise\n";
+        show_dot_matrix(length($line1), length($line2), \@dot_matrix, "dot_matrix_with_noise");
+        print "\nDo you want to clear noise? (y/n): ";
+        my $choice = <>;
+        chomp $choice;
+        if(lc($choice) eq "y" || lc($choice) eq "Y"){
+            clear_noise(length($line1), length($line2), \@dot_matrix);
+            show_dot_matrix(length($line1), length($line2), \@dot_matrix, "dot_matrix_with_deleted_noise");
+        }
     }
 }while($line1 && $line2);
